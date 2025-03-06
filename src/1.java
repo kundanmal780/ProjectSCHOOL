b10509ffb6f615463fd4ba9e32dc11fe10325221
kundanmal780 <kundanmal780@gmail.com>
2025-03-06 09:48:34 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ProjectSCHOOL {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = scan.nextLine();
+		System.out.println("Hello " + name);
+	}
+}

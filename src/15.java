37b508c3f619090ad4dececfdead829fb7718a7a
kundanmal780 <kundanmal780@gmail.com>
2025-03-20 11:26:17 +0000

    Add 15.java

---
+++ src/15.java
@@ -0,0 +1,6 @@
+public class SchoolProject {
+    public static void main(String[] args) {
+        System.out.println("Welcome to ProjectSCHOOL!");
+        // TODO: Implement your project here
+    }
+}

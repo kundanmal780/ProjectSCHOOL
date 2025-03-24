21fd9cd72c69d438ed2bd82c514becb7bb170d11
kundanmal780 <kundanmal780@gmail.com>
2025-03-24 16:35:11 +0000

    Add 19.java

---
+++ src/19.java
@@ -0,0 +1,11 @@
+public class SchoolProject {
+    public void addStudent(String studentName) {
+        // Add the student name to the project
+        System.out.println("Added " + studentName);
+    }
+
+    public void addTeacher(String teacherName) {
+        // Add the teacher name to the project
+        System.out.println("Added " + teacherName);
+    }
+}

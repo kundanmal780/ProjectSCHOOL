928273712a51663cd2087181446bacafa8db10f6
kundanmal780 <kundanmal780@gmail.com>
2025-04-06 13:10:05 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,8 @@
+public class YourClassName {
+    // Code goes here
+
+    public static void main(String[] args) {
+        // Main method logic goes here
+        System.out.println("Hello, World!");
+    }
+}

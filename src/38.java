ac420700247cedd0646cf92863610206ed09f6d1
kundanmal780 <kundanmal780@gmail.com>
2025-04-06 14:10:01 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,20 @@
+public class StudentProject {
+
+    private String projectName;
+
+    public StudentProject(String project) {
+        this.projectName = project;
+    }
+
+    public void addTask(Task task) {
+        // Add task to the project
+    }
+
+    public void scheduleTasks() {
+        // Schedule tasks for the current day
+    }
+
+    public void deleteTask(int taskId) {
+        // Delete a specific task from the project
+    }
+}

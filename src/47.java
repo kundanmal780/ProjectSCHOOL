f536e94c381963b53eedadd8abf3e14eb429d84d
kundanmal780 <kundanmal780@gmail.com>
2025-04-07 14:37:41 +0000

    Add 47.java

---
+++ src/47.java
@@ -0,0 +1,6 @@
+public class ProjectSCHOOL {
+    public static void main(String[] args) {
+        // Code to initialize projectSCHOOL here
+        System.out.println("Project SCHEDULE has started.");
+    }
+}

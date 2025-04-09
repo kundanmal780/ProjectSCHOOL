9b89a32c900636709625b08f1893da46a280d769
kundanmal780 <kundanmal780@gmail.com>
2025-04-09 18:03:21 +0000

    Add 49.java

---
+++ src/49.java
@@ -0,0 +1,12 @@
+public class ProjectSCHOOL {
+    public static void main(String[] args) {
+        // Example of how you might want to handle user input in your project
+        System.out.println("Please enter some details about your project:");
+        String detail = scan.nextLine();
+        System.out.println("You've entered: " + detail);
+    }
+
+    private static String scan() {
+        return promptUserForInput();
+    }
+}
